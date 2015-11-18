9c0506ac6f342779bcbcee716fbf8fdfe0432fa6
Daniel Marchena <daniel@Daniels-MacBook-Pro.local>
2015-11-18 21:21:09 -0200

    Adicionando minimo de codigo para executar testQtdeHorasRestantes

---
+++ Gerador Burndown/src/br/com/tcc/bo/BurnupBO.java
@@ -0,0 +1,13 @@
+package br.com.tcc.bo;
+
+public class BurnupBO {
+	
+	public double getQtdeHorasRestantes() {
+		return 0;
+	}
+
+	public void setQtdeHorasRestantes(double qtdeHorasRestantes) {
+		
+	}
+			
+}
