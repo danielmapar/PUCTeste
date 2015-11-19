9a0352e3f2e397fbda1a4839f6c202b8c8bb80ec
danielmapar <danielmapar@gmail.com>
2015-11-19 10:12:48 -0200

    Criando teste - testGerarBurnupHoras

--- Gerador Burndown/src/br/com/tcc/bo/BurnupBOTest.java
+++ Gerador Burndown/src/br/com/tcc/bo/BurnupBOTest.java
@@ -2,9 +2,47 @@ package br.com.tcc.bo;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import br.com.tcc.model.Sprint;
+import br.com.tcc.util.DataUtil;
+import br.com.tcc.util.Main;
+
 public class BurnupBOTest {
+	
+	private Sprint sprint1;
+	
+	private Sprint criaSprint(final String DT_INICIO_SPRINT, final int QTDE_DIAS_SPRINT) throws ParseException {
+		// Cria uma instancia valida de Sprint
+		Sprint sprint = new Sprint();
+		sprint.setDtInicio(DataUtil.converteStringParaDate(DT_INICIO_SPRINT));
+		sprint.setQtdeDias(QTDE_DIAS_SPRINT);
+		sprint.setEstorias(Main.criaListaEstorias());
+		
+		sprint.setTotalHoras(new SprintBO().calculaTotalHoras(sprint));
+						
+		//cria historico de entradas
+		sprint.setItensHistorico(Main.criaItensHistorico());
+		
+		return sprint;
+	}
+	
+	@Before 
+	public void initialize() throws ParseException {
+		// Cria uma instancia valida de Sprint
+		this.sprint1 = criaSprint("08/09/2014", 7);
+	}
+	
+	@Test
+	public void testGerarBurnupHoras() {
+		new BurnupBO().gerarBurnupHoras(this.sprint1);
+	}
 	
 	@Test
 	public void testQtdeHorasRestantes() {
@@ -33,6 +71,12 @@ public class BurnupBOTest {
 		burnupBO.setConsumoDiarioIdeal(10);
 		assertEquals((Double)10.00, (Double) burnupBO.getConsumoDiarioIdeal());
 	}
+	
+	@After 
+	public void clear() {
+		// Destruir instancias 
+		this.sprint1 = null;
+	}	
 	
 
 }
